5d4c3d97f967acc501078f74363dedbd901ec056
Shreyaschorge <shreyaschorge81@gmail.com>
2022-12-26 23:17:50 +0530

    client setup

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+
+  // const fetchProduct = async () => {
+  //   try {
+  //     const res = await axios.get('/api/products');
+  //     console.log(res)
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   fetchProduct();
+  // }, [])
+
+  return (
+    <div className="App">
+      <h2>Hello World</h2>
+    </div>
+  );
+}
+
+export default App;
